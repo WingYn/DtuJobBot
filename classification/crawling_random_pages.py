4926705f561c896a723bf10b51ff4916a3ff5184
Helge Munk Jacobsen <helgemunkjacobsen@gmail.com>
2012-11-19 23:12:25 +0100

    crawling google now works without being blocked

--- classification/crawling_random_pages.py
+++ classification/crawling_random_pages.py
@@ -25,16 +25,18 @@ import time
 
 # Continue where we left
 try:
-    Q, url_list = pickle.load(open("url_list.pydata", "rb" ))
+    counter = pickle.load(open("counter.pydata", "rb" )) - 1
+    Q, url_list = pickle.load(open("url_list" + str(counter) + ".pydata", "rb" ))
 except IOError:
     Q = "DTU"
     url_list = []
+    counter = 0
 
-while len(url_list) < 10000:
+while len(url_list) < 12000:
     # Sending GET request
     start = random.randint(1, 2) * 10
     url = 'https://www.google.com/search?q=%s&lr=lang_en&pws=0&safe=off&start=%d' % (Q, start)
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'    
     headers={'User-Agent':user_agent,}
     request = urllib2.Request(url, None, headers)
     html = urllib2.urlopen(request).read()
@@ -52,7 +54,13 @@ while len(url_list) < 10000:
         Q = ''.join(random.sample([l for l in 'abcdefghijklmnopqrstuvwxyz'],3))
         print "Now searching for %r" % Q
     # Backup data in case of an error
-    pickle.dump((Q,url_list),open("url_list.pydata", "wb" ))
+    pickle.dump((Q,url_list),open("url_list" + str(counter) + ".pydata", "wb" ))
     print "Length of url_list: %d, Last search query: %r" % (len(url_list), Q)
-    time.sleep(3)
+    
+    # Every 4th time wait 1 minute and 15 seconds in average
+    time.sleep(random.randint(0, 1) * random.randint(0, 1) * random.randint(0, 250))
 
+    # Change the filename for every 90 url
+    if (len(url_list) % 90 == 0):
+        counter += 1
+        pickle.dump(counter,open("counter.pydata", "wb" ))
